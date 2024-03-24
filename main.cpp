7df6ce3a29b69102579052e2f31d67885b4d44c8
SilverNevis <145479006+SilverNevis@users.noreply.github.com>
2024-03-24 17:45:39 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <mutex>
+
+struct Node
+{
+    int value;
+    Node* next;
+    std::mutex* node_mutex;
+
+    Node(int val) : value(val), next(nullptr), node_mutex(new std::mutex()) {}
+    ~Node() {
+        delete node_mutex;
+    }
+};
+
+class FineGrainedQueue
+{
+private:
+    Node* head;
+    std::mutex* queue_mutex;
+
+public:
+    FineGrainedQueue() : head(nullptr), queue_mutex(new std::mutex()) {}
+    ~FineGrainedQueue() {
+        delete queue_mutex;
+    }
+
+    
+    void insertIntoMiddle(int value, int pos) {
+        std::lock_guard<std::mutex> queue_lock(*queue_mutex);
+
+        if (head == nullptr) { 
+            head = new Node(value);
+            return;
+        }
+
+        Node* prev = head;
+
+        
+        for (int i = 0; i < pos - 1 && prev->next != nullptr; ++i) {
+            prev = prev->next;
+        }
+
+        std::lock_guard<std::mutex> prev_lock(*prev->node_mutex);
+
+        Node* newNode = new Node(value);
+
+    
+        newNode->next = prev->next;
+        prev->next = newNode;
+    }
+};
+
+int main() {
+    FineGrainedQueue queue;
+
+    
+    queue.insertIntoMiddle(10, 1); 
+
+    return 0;
+}
+
+
